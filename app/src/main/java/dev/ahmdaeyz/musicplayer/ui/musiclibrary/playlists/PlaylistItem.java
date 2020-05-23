4b13251469414091bb9ded38f30fd897a9c262c5
ahmdaeyz <ahmdaeyz@gmail.com>
2020-05-23 16:43:43 +0200

    Can now open playlist details with the embedded play button on the playlist item instead of long pressing

--- app/src/main/java/dev/ahmdaeyz/musicplayer/ui/musiclibrary/playlists/PlaylistItem.java
+++ app/src/main/java/dev/ahmdaeyz/musicplayer/ui/musiclibrary/playlists/PlaylistItem.java
@@ -1,16 +1,22 @@
 package dev.ahmdaeyz.musicplayer.ui.musiclibrary.playlists;
 
-import android.view.LayoutInflater;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 
+import com.skydoves.transformationlayout.TransformationCompat;
 import com.xwray.groupie.viewbinding.BindableItem;
+
+import org.parceler.Parcels;
 
 import dev.ahmdaeyz.musicplayer.R;
 import dev.ahmdaeyz.musicplayer.databinding.PlaylistItemBinding;
 import dev.ahmdaeyz.musicplayer.model.Playlist;
+import dev.ahmdaeyz.musicplayer.ui.playlistdetails.PlaylistDetailsActivity;
+
+import static dev.ahmdaeyz.musicplayer.ui.musiclibrary.playlists.PlaylistsFragment.PLAYLIST_ARG;
 
 public class PlaylistItem extends BindableItem<PlaylistItemBinding> {
 
@@ -30,10 +36,14 @@ public class PlaylistItem extends BindableItem<PlaylistItemBinding> {
     public void bind(@NonNull PlaylistItemBinding viewBinding, int position) {
         viewBinding.playlistName.setText(playlist.getName());
         // TODO: create a simple college of the first 4 songs of the playlist
-        viewBinding.getRoot().setOnClickListener(new View.OnClickListener() {
+        viewBinding.playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(viewBinding.getRoot().getContext(), PlaylistDetailsActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putParcelable(PLAYLIST_ARG, Parcels.wrap(playlist));
+                intent.putExtras(bundle);
+                TransformationCompat.INSTANCE.startActivity(viewBinding.transformationLayout, intent);
             }
         });
     }
